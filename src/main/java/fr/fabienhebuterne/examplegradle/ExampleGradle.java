6bee77d27730245ac9c8bb7cf30f6e2050a31ebb
Fabien <contact@fabien-hebuterne.fr>
2020-11-11 14:49:02 +0100

    init

---
+++ src/main/java/fr/fabienhebuterne/examplegradle/ExampleGradle.java
@@ -0,0 +1,16 @@
+package fr.fabienhebuterne.examplegradle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExampleGradle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
